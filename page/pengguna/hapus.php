29872d9c6c52fd33a759e602e222fbdc9aa7c2c3
Chelsy el <95273591+Duckip@users.noreply.github.com>
2022-06-04 00:26:26 +0800

    Add files via upload

--- page/pengguna/hapus.php
+++ page/pengguna/hapus.php
@@ -1,1 +1,15 @@
+<?php 
 
+		$id=$_GET['id'];
+		$sql=$koneksi->query("delete from tb_user where id='$id'");
+		if ($sql) {
+			?>
+
+          		<script type="text/javascript">
+          		alert("Data Berhasil Dihapus");
+          		window.location.href="?page=pengguna";
+          		</script>
+
+       		<?php 
+		}
+ ?>
