68151ecd75606e194091ff69c446dcd957df21ea
Denise Valeria <92726846+DeniseVal@users.noreply.github.com>
2022-06-03 17:52:27 +0800

    Update hapus.php

--- page/pelanggan/hapus.php
+++ page/pelanggan/hapus.php
@@ -1,1 +1,15 @@
+<?php 
 
+		$kode=$_GET['id'];
+		$sql=$koneksi->query("delete from tb_pelanggan where kode_pelanggan='$kode'");
+		if ($sql) {
+			?>
+
+          		<script type="text/javascript">
+          		alert("Data Berhasil Dihapus");
+          		window.location.href="?page=pelanggan";
+          		</script>
+
+       		<?php 
+		}
+ ?>
